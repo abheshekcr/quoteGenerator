826eb13d45526952f5c4745fdd18ed4681f2bdba
abheshekcr <cabheshek@gmail.com>
2020-05-25 13:37:33 +0530

    simple chang

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={RandomGen}/>
+        <Route path="/"component={RandomGen}/>
       </Router>
    
     </div>
